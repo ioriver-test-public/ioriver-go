1edfa454547e28077b04cc68341551f002069992
Maayan Ben David <maayan@ioriver.io>
2023-11-01 10:59:12 +0200

    fixing pipelines

---
+++ version/version.go
@@ -0,0 +1,23 @@
+// version/version.go
+package version
+
+import (
+    "fmt"
+    "time"
+)
+
+var (
+    Major          = 1
+    Minor          = 0
+    Patch          = 0
+    CommitHash     = "n/a"
+    BuildTimestamp = "n/a"
+)
+
+func BuildVersion() string {
+    return fmt.Sprintf("%d.%d.%d-%s (%s)", Major, Minor, Patch, CommitHash, BuildTimestamp)
+}
+
+func IncrementMinorVersion() {
+    Minor++
+}
